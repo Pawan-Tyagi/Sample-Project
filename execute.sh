c66ee9d869b1ab6343aea81ea9cd4cb18b5e758e
Abhinav Gupta <abhinavguptas@users.noreply.github.com>
2017-08-14 19:01:56 +0530

    Update execute.sh

--- execute.sh
+++ execute.sh
@@ -1,3 +1,9 @@
+rm -rf gen
+
+mkdir gen
+
+javac -d gen -cp "./lib/*" src/com/cmd/*.java 
+
 echo "--------------------------------------------------"
 echo "Current Directory listing"
 echo "--------------------------------------------------"
@@ -13,4 +19,4 @@ echo "--------------------------------------------------"
 echo "Fired the core command"
 echo "--------------------------------------------------"
 
-java -cp ".:./lib/*:./dist/*" org.testng.TestNG testng.xml
+java -cp ".:./lib/*:./gen/" org.testng.TestNG testng.xml
