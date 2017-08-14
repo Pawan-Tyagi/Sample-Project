7f93b4bd41ddd56e2b22291a9dda12f2131c19b6
Pawan-Tyagi <30889577+Pawan-Tyagi@users.noreply.github.com>
2017-08-14 17:49:00 +0530

    Update execute.sh

--- execute.sh
+++ execute.sh
@@ -1,1 +1,1 @@
-java -cp .:Selenium\Concretio\SampleCMD\bin;C:\Selenium\Concretio\SampleCMD\lib\* org.testng.TestNG testng.xml
+java -cp ".:Selenium\Concretio\SampleCMD\bin:C:\Selenium\Concretio\SampleCMD\lib\*" org.testng.TestNG testng.xml
